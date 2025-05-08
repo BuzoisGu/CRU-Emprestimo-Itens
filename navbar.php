68aee4988e543b3d0a7747eb6b118e9d4d455c93
João Vitor Colombo <jvcolombo.06@gmail.com>
2025-05-07 21:04:53 -0300

    refactor: organizando pastas

--- navbar.php
+++
@@ -1,7 +0,0 @@
-<nav class="navbar navbar-dark bg-dark">
-    <div class="container-md">
-        <a href="#" class="navbar-brand">
-            Empréstimo de Itens
-        </a>
-    </div>
-</nav>
